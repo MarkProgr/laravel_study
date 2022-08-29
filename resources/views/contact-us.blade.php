74d30401bdd02a074c150560e8fdedebc9138465
Mark Kaladzinsky <kristianomark7@gmail.com>
2022-08-29 14:40:45 +0300

    fixed laravel hw_1

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -9,8 +9,8 @@
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputPassword1" class="form-label">Name</label>
+            <input type="text" class="form-control" id="exampleInputPassword1">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Phone number</label>
